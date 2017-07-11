c0a2162b38b11904d2f2493eee0ca75b96ec593f
froth30 <theo.frohlich@gmail.com>
2017-07-11 13:33:12 -0400

    Start daily challenge: racers

---
+++ src/main/scala/com/codefights/racers.scala
@@ -0,0 +1,12 @@
+package com.codefights
+
+/**
+  * Created by froth30 on 7/11/2017.
+  */
+object racers {
+    
+    def racers(track: Int, accel: Array[Int], spd: Array[Int]): Array[Int] = {
+    
+    }
+    
+}
