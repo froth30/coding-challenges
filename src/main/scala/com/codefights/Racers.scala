629a9d48ad6fb44ff810417c7b5d79a603523e90
froth30 <theo.frohlich@gmail.com>
2017-07-12 05:03:01 -0400

    Implement daily challenge: racers

--- src/main/scala/com/codefights/Racers.scala
+++ src/main/scala/com/codefights/Racers.scala
@@ -6,7 +6,40 @@ package com.codefights
 object Racers {
     
     def racers(track: Int, accel: Array[Int], spd: Array[Int]): Array[Int] = {
+        spd.indices.zip(accel zip spd)
+                .sortBy(p => timeToReachDistance(track, p._2._1, p._2._2))
+                .map(_._1).toArray
+    }
     
+    def timeToReachDistance(dist: Double, accel: Double, spd: Double): Double = {
+        val distLeft = dist - distanceToReachTopSpeed(accel, spd)
+        if (distLeft > 0) {
+            val timeLeft = distLeft / spd
+            timeToReachTopSpeed(accel, spd) + timeLeft
+        }
+        else timeToReachDistance(dist, accel)
+    }
+    
+    def distanceToReachTopSpeed(accel: Double, spd: Double): Double = {
+        // v^2 = v0^2 + 2a(x - x0)
+        // v^2 = 2ax
+        // x = v^2 / 2a
+        math.pow(spd, 2) / (2 * accel)
+    }
+    
+    def timeToReachTopSpeed(accel: Double, spd: Double): Double = {
+        // v = v0 + at
+        // v = at
+        // t = v/a
+        spd / accel
+    }
+    
+    def timeToReachDistance(dist: Double, accel: Double): Double = {
+        // x = x0 + v0t + 1/2 at^2
+        // x = 1/2 at^2
+        // t^2 = 2x/a
+        // t = sqrt(2x/a)
+        math.sqrt(2 * dist / accel)
     }
     
 }
