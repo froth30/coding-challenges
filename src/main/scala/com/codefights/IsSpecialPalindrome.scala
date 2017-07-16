12c7ff5ea9775c414e1154d3858df730a19db48a
froth30 <theo.frohlich@gmail.com>
2017-07-16 18:15:51 -0400

    Finalize code for submission

--- src/main/scala/com/codefights/IsSpecialPalindrome.scala
+++ src/main/scala/com/codefights/IsSpecialPalindrome.scala
@@ -7,7 +7,8 @@ object IsSpecialPalindrome {
     
     def isSpecialPalindrome(n: Int) = {
         val s = n toBinaryString
-        
+    
+        // More efficient to use short-circuiting '&&' operator here, but doing so costs an extra 2 chars
         s == s.reverse &
         s.count(_ == '0') == 1 &
         s.contains('1')
