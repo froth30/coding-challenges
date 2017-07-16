6cbc58600a78b7646aa36ec70cda5d66c90b721c
froth30 <theo.frohlich@gmail.com>
2017-07-16 17:43:48 -0400

    Implement daily challenge: isSpecialPalindrome

--- src/main/scala/com/codefights/IsSpecialPalindrome.scala
+++ src/main/scala/com/codefights/IsSpecialPalindrome.scala
@@ -6,7 +6,14 @@ package com.codefights
 object IsSpecialPalindrome {
     
     def isSpecialPalindrome(n: Int): Boolean = {
-        ???
+        val s = n.toBinaryString
+        
+        s.count(_ == '0') == 1 && s.contains('1') && {
+            for (i <- 0 to s.length / 2)
+                if (s.charAt(i) != s.charAt(s.length-1-i))
+                    return false
+            true
+        }
     }
     
 }
