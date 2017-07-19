9407188e32b64eda3b7ca97af3ce383608ced481
froth30 <theo.frohlich@gmail.com>
2017-07-19 02:33:21 -0400

    Begin CodeFights Challenge `nextGameOfLife`

---
+++ src/main/java/com/codefights/NextGameOfLife.java
@@ -0,0 +1,19 @@
+package com.codefights;
+
+/**
+ * CodeFights Challenge `nextGameOfLife`
+ *
+ * @since Jul 19, 2017
+ * @see <a href="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life">Conway's Game of Life</a>
+ * @author froth30
+ */
+public class NextGameOfLife {
+    
+    public static int[][] nextGameOfLife(int[][] seed) {
+        
+        // TODO
+        
+        return seed;
+    }
+    
+}
