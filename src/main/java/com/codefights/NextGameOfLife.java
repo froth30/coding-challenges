d3c495034b68bfb3cd21de6af4f910c42c278fae
froth30 <theo.frohlich@gmail.com>
2017-07-20 03:24:58 -0400

    Implement rough, incomplete solution

--- src/main/java/com/codefights/NextGameOfLife.java
+++ src/main/java/com/codefights/NextGameOfLife.java
@@ -10,10 +10,31 @@ package com.codefights;
 public class NextGameOfLife {
     
     public static int[][] nextGameOfLife(int[][] seed) {
+        int[][] next = seed.clone();
+        int row, col, nrows = seed.length, ncols = seed[0].length,
+                liveNeighbors, I[] = {-1, 0, 1};
         
-        // TODO
+        for (row = 0; row < nrows; row++) {
+            for (col = 0; col < ncols; col++) {
+                liveNeighbors = 0;
+                for (int i : I) {
+                    for (int j : I) {
+                        if (!(i == 0 && j == 0)) {
+                            liveNeighbors += seed[row + i][col + j];
+                        }
+                    }
+                }
+                
+                if (seed[row][col] == 1) {
+                    if (liveNeighbors < 2 || liveNeighbors > 3)
+                        next[row][col] = 0;
+                } else if (liveNeighbors == 3) {
+                    next[row][col] = 1;
+                }
+            }
+        }
         
-        return seed;
+        return next;
     }
     
 }
