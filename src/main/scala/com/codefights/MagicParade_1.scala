549ffd194532a50a14781683a20a2816d161e025
froth30 <theo.frohlich@gmail.com>
2017-07-20 15:38:41 -0400

    Begin CodeFights Challenge `magicParade_1`

---
+++ src/main/scala/com/codefights/MagicParade_1.scala
@@ -0,0 +1,15 @@
+package com.codefights
+
+/**
+  * CodeFights Challenge `magicParade_1`
+  *
+  * @since Jul 20, 2017
+  * @author froth30
+  */
+object MagicParade_1 {
+    
+    def magicParade_1(water: Int, fire: Int, death: Int): Boolean = {
+        ???
+    }
+    
+}
