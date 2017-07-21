37f70dd61871077b4d44202b6f9edef751b2297b
froth30 <theo.frohlich@gmail.com>
2017-07-21 07:06:57 -0400

    Set 'timeout' attribute in @Test() annotation

--- src/test/scala/com/codefights/MagicParade_1_Test.scala
+++ src/test/scala/com/codefights/MagicParade_1_Test.scala
@@ -13,49 +13,49 @@ class MagicParade_1_Test {
     
     import MagicParade_1._
     
-    @Test
+    @Test(timeout = 20000)
     def test01(): Unit = assert(magicParade_1(1, 0, 2) == false)
     
-    @Test
+    @Test(timeout = 20000)
     def test02(): Unit = assert(magicParade_1(1, 0, 1) == true)
     
-    @Test
+    @Test(timeout = 20000)
     def test03(): Unit = assert(magicParade_1(0, 4, 2) == true)
     
-    @Test
+    @Test(timeout = 20000)
     def test04(): Unit = assert(magicParade_1(0, 5, 1) == false)
     
-    @Test
+    @Test(timeout = 20000)
     def test05(): Unit = assert(magicParade_1(1, 5, 1) == true)
     
-    @Test
+    @Test(timeout = 20000)
     def test06(): Unit = assert(magicParade_1(0, 0, 0) == false)
     
-    @Test
+    @Test(timeout = 20000)
     def test07(): Unit = assert(magicParade_1(5, 25, 4) == true)
     
-    @Test
+    @Test(timeout = 20000)
     def test08(): Unit = assert(magicParade_1(20, 20, 22) == false)
     
-    @Test
+    @Test(timeout = 20000)
     def test09(): Unit = assert(magicParade_1(100000000, 100000000, 100000000) == true)
     
-    @Test
+    @Test(timeout = 20000)
     def test10(): Unit = assert(magicParade_1(5, 38, 10) == false)
     
-    @Test
+    @Test(timeout = 20000)
     def test11(): Unit = assert(magicParade_1(3, 21, 5) == true)
     
-    @Test
+    @Test(timeout = 20000)
     def test12(): Unit = assert(magicParade_1(0, 0, 1) == true)
     
-    @Test
+    @Test(timeout = 20000)
     def test13(): Unit = assert(magicParade_1(10000000, 100000000, 10000000) == false)
     
-    @Test
+    @Test(timeout = 20000)
     def test14(): Unit = assert(magicParade_1(10000000, 10000000, 100000000) == false)
     
-    @Test
+    @Test(timeout = 20000)
     def test15(): Unit = assert(magicParade_1(10000000, 50000000, 10000000) == true)
     
 }
