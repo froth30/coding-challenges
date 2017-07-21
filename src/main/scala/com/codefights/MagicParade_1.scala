6f4d89afb5e93182c998dfa6ce8b028309e2702b
froth30 <theo.frohlich@gmail.com>
2017-07-21 07:21:00 -0400

    Achieve half-correct solution

--- src/main/scala/com/codefights/MagicParade_1.scala
+++ src/main/scala/com/codefights/MagicParade_1.scala
@@ -9,7 +9,25 @@ package com.codefights
 object MagicParade_1 {
     
     def magicParade_1(water: Int, fire: Int, death: Int): Boolean = {
-        ???
+        isSafeParade("", water, fire, death)
+    }
+    
+    private def isSafeParade(parade: String, water: Int, fire: Int, death: Int): Boolean = {
+        if (water < 0 || fire < 0 || death < 0)
+            false
+
+        else if (water == 0 && fire == 0 && death == 0)
+            parade.nonEmpty
+
+        else if (parade.nonEmpty && parade.last == 'D')
+             isSafeParade(parade + "F", water, fire - 1, death)
+            
+        else if (parade.length() >= 3 && parade.endsWith("FFF"))
+             isSafeParade(parade + "W", water - 1, fire, death)
+            
+        else isSafeParade(parade + "W", water - 1, fire, death) ||
+             isSafeParade(parade + "F", water, fire - 1, death) ||
+             isSafeParade(parade + "D", water, fire, death - 1)
     }
     
 }
