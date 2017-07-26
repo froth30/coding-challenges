01d94e1be459bce6e6a2df427a238040e2d17a0e
froth30 <theo.frohlich@gmail.com>
2017-07-26 12:14:34 -0400

    Begin CodeFights Challenge `raiseIt`

---
+++ src/main/scala/com/codefights/RaiseIt.scala
@@ -0,0 +1,15 @@
+package com.codefights
+
+/**
+  * CodeFights Challenge `raiseIt`
+  *
+  * @since Jul 26, 2017
+  * @author froth30
+  */
+object RaiseIt {
+    
+    def raiseIt(arg1: Array[Int]): Int = {
+        ???
+    }
+    
+}
