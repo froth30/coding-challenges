89d1f3fbe3e24e64442aca4a5b64c734d8509c53
froth30 <theo.frohlich@gmail.com>
2017-07-28 12:30:29 -0400

    Begin CodeFights Challenge `gold`

---
+++ src/main/scala/com/codefights/Gold.scala
@@ -0,0 +1,15 @@
+package com.codefights
+
+/**
+  * CodeFights Challenge `gold`
+  *
+  * @since Jul 28, 2017
+  * @author froth30
+  */
+object Gold {
+    
+    def gold(n: Int): Int = {
+        ???
+    }
+    
+}
