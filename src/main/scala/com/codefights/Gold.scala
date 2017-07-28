214df728ad850d7a7e840a98cb3250e9af73de63
froth30 <theo.frohlich@gmail.com>
2017-07-28 13:16:33 -0400

    Simplify CodeFights Challenge `gold` for submission

--- src/main/scala/com/codefights/Gold.scala
+++ src/main/scala/com/codefights/Gold.scala
@@ -8,14 +8,12 @@ package com.codefights
   */
 object Gold {
     
-    def gold(n: Int): Int = {
-        (2 to n/2).map(k =>
-            if (isPrime(k) && isPrime(n-k)) 1
+    def gold(n: Int) =
+        (2 to n/2) map(k =>
+            if (p(k) & p(n-k)) 1
             else 0
-        ).sum
-    }
+        ) sum
     
-    def isPrime(n: Int): Boolean =
-        ! ((2 to n-2) exists (n % _ == 0))
+    def p(n: Int) = ! ((2 to n-2) exists (n % _ == 0))
     
 }
