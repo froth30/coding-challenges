d0bc4139953f6e6d7dbadb9398d63435d4515267
froth30 <theo.frohlich@gmail.com>
2017-07-28 13:22:26 -0400

    Omit unnecessary parentheses around ranges

--- src/main/scala/com/codefights/Gold.scala
+++ src/main/scala/com/codefights/Gold.scala
@@ -9,11 +9,11 @@ package com.codefights
 object Gold {
     
     def gold(n: Int) =
-        (2 to n/2) map(k =>
+        2 to n/2 map(k =>
             if (p(k) & p(n-k)) 1
             else 0
         ) sum
     
-    def p(n: Int) = ! ((2 to n-2) exists (n % _ == 0))
+    def p(n: Int) = ! (2 to n-2 exists (n % _ == 0))
     
 }
