d6703eb0125c09d0424e7f7503db555f00698167
froth30 <theo.frohlich@gmail.com>
2017-07-30 18:40:18 -0400

    Achieve working implementation

--- src/main/scala/com/codefights/RotateArrayFromIndex.scala
+++ src/main/scala/com/codefights/RotateArrayFromIndex.scala
@@ -9,7 +9,22 @@ package com.codefights
 object RotateArrayFromIndex {
     
     def rotateArrayFromIndex(values: Array[Int], start: Int, end: Int): Array[Int] = {
-        ???
+        var trueStart = math.min(start, values.length - 1)
+        var trueEnd   = math.min(end,   values.length - 1)
+        var interval  = values
+        
+        if (end < start) {
+            val temp = trueStart
+            trueStart = trueEnd
+            trueEnd = temp
+            interval = values(trueEnd) +: values.slice(trueStart, trueEnd)
+        } else {
+            interval = values.slice(trueStart + 1, trueEnd + 1) :+ values(trueStart)
+        }
+        
+        values.take(trueStart) ++
+            interval ++
+            values.slice(trueEnd + 1, values.length)
     }
     
 }
