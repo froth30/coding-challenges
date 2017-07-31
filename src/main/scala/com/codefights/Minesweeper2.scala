9d98e63cd7d8fa8dc9a559fd17abbd88ae323a66
froth30 <theo.frohlich@gmail.com>
2017-07-31 15:18:36 -0400

    Begin CodeFights Challenge `minesweeper2`

---
+++ src/main/scala/com/codefights/Minesweeper2.scala
@@ -0,0 +1,15 @@
+package com.codefights
+
+/**
+  * CodeFights Challenge `minesweeper2`
+  *
+  * @since Jul 31, 2017
+  * @author froth30
+  */
+object Minesweeper2 {
+    
+    def minesweeper2(gameboard: Array[Array[Int]], opening: Array[Array[Boolean]], moves: Array[Array[Int]]): Array[Array[Boolean]] = {
+        ???
+    }
+    
+}
