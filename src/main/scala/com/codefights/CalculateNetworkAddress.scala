705a964d0aa66f235d13459450bed073019d9dbe
froth30 <theo.frohlich@gmail.com>
2017-08-05 14:56:54 -0400

    Prepend method with proper documentation

--- src/main/scala/com/codefights/CalculateNetworkAddress.scala
+++ src/main/scala/com/codefights/CalculateNetworkAddress.scala
@@ -8,6 +8,12 @@ package com.codefights
   */
 object CalculateNetworkAddress {
     
+    /**
+      * Calculates the network address of an IP address in CIDR notation.
+      *
+      * @param cidr The string in the format "n1.n2.n3.n4/m", where all n-s are 0 ≤ n ≤ 255 and 0 ≤ m ≤ 32.
+      * @return The network address of the given CIDR notation.
+      */
     def calculateNetworkAddress(cidr: String): String = {
         ???
     }
