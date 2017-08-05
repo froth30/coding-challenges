92766e087e5aa8bfa0ab229b87580bc5b71fbd9a
froth30 <theo.frohlich@gmail.com>
2017-08-05 14:50:28 -0400

    Begin CodeFights Challenge `calculateNetworkAddress`

---
+++ src/main/scala/com/codefights/CalculateNetworkAddress.scala
@@ -0,0 +1,15 @@
+package com.codefights
+
+/**
+  * CodeFights Challenge `calculateNetworkAddress`
+  *
+  * @since Aug 5, 2017
+  * @author froth30
+  */
+object CalculateNetworkAddress {
+    
+    def calculateNetworkAddress(cidr: String): String = {
+        ???
+    }
+    
+}
