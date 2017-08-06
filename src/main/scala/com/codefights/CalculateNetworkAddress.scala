6168e4754581d5c1746232bf57f08a5028c84f36
froth30 <theo.frohlich@gmail.com>
2017-08-06 00:56:15 -0400

    Reduce solution for submission

--- src/main/scala/com/codefights/CalculateNetworkAddress.scala
+++ src/main/scala/com/codefights/CalculateNetworkAddress.scala
@@ -11,25 +11,25 @@ object CalculateNetworkAddress {
     /**
       * Calculates the network address of an IP address in CIDR notation.
       *
-      * @param cidr The string in the format "n1.n2.n3.n4/m", where all n-s are 0 ≤ n ≤ 255 and 0 ≤ m ≤ 32.
+      * @param c The string in the format "n1.n2.n3.n4/m", where all n-s are 0 ≤ n ≤ 255 and 0 ≤ m ≤ 32.
       * @return The network address of the given CIDR notation.
       */
-    def calculateNetworkAddress(cidr: String): String = {
-        val components = cidr.split('/')
-        val blocks = components(0).split('.')
-        val bits = blocks.map(_.toInt.toBinaryString.toInt.formatted("%08d")).mkString
-        val mask = components(1).toInt
-        var networkAddress = bits.toArray
+    def calculateNetworkAddress(c: String) = {
+        val p = c split '/'
+        val B = p(0) split '.'
+        val b = B.map(_.toInt.toBinaryString.toInt.formatted("%08d")) mkString
+        val m = p(1) toInt
+        var n = b toArray
         
-        (1 to mask).foreach(i => networkAddress(i-1) = bits(i-1))
-        (mask to 31).foreach(networkAddress(_) = '0')
+        1 to m foreach(i => n(i-1) = b(i-1))
+        m to 31 foreach(n(_) = 48)
         
-        (0 to 3).foreach(i => {
-            blocks(i) = Integer.parseInt(networkAddress.take(8).mkString, 2).toString
-            networkAddress = networkAddress.drop(8)
+        0 to 3 foreach(i => {
+            B(i) = Integer.parseInt(n take 8 mkString, 2).toString
+            n = n drop 8
         })
         
-        blocks.mkString(".")
+        B mkString "."
     }
     
 }
