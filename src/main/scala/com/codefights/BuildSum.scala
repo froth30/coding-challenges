73ec82b8edd7f8f6a5a164cff86d556b732b2e5b
froth30 <theo.frohlich@gmail.com>
2017-08-06 13:46:13 -0400

    Begin CodeFights Challenge `buildSum`

---
+++ src/main/scala/com/codefights/BuildSum.scala
@@ -0,0 +1,15 @@
+package com.codefights
+
+/**
+  * CodeFights Challenge `buildSum`
+  *
+  * @since Aug 6, 2017
+  * @author froth30
+  */
+object BuildSum {
+    
+    def buildSum(n: Int): Int = {
+        ???
+    }
+    
+}
