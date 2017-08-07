6567ce5fa4393a54e6f127b4d74777205afeb6ea
froth30 <theo.frohlich@gmail.com>
2017-08-06 21:29:25 -0400

    Achieve shorter solution

--- src/main/java/com/codefights/BuildSum_Java.java
+++ src/main/java/com/codefights/BuildSum_Java.java
@@ -10,8 +10,8 @@ public class BuildSum_Java {
     
     public static int buildSum(int n) {
         int s = 1;
-        while (n-- > 0)
-            s += Math.pow(n,n);
+        while (n > 0)
+            s += Math.pow(n, n--);
         
         return s;
     }
