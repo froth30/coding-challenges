57ef7f658564d28361393518997243bcd49920af
froth30 <theo.frohlich@gmail.com>
2017-08-06 21:22:38 -0400

    Rename class to differentiate from Scala singleton object

--- src/main/java/com/codefights/BuildSum_Java.java
+++ src/main/java/com/codefights/BuildSum_Java.java
@@ -6,13 +6,13 @@ package com.codefights;
  * @since Aug 6, 2017
  * @author froth30
  */
-class BuildSum {
+public class BuildSum_Java {
     
     public static int buildSum(int n) {
         int s = 1;
-        for (int i = 1; i <= n; i++) {
-            s += Math.pow(i,i);
-        }
+        while (n-- > 0)
+            s += Math.pow(n,n);
+        
         return s;
     }
     
