2e7a7d4d6a3175218b5e75d326df6d991a38ed8e
froth30 <theo.frohlich@gmail.com>
2017-08-06 21:06:19 -0400

    Try CodeFights Challenge `buildSum` in Java

---
+++ src/main/java/com/codefights/BuildSum.java
@@ -0,0 +1,15 @@
+package com.codefights;
+
+/**
+ * CodeFights Challenge `buildSum`
+ *
+ * @since Aug 6, 2017
+ * @author froth30
+ */
+class BuildSum {
+    
+    public static int buildSum(int n) {
+        return -1;
+    }
+    
+}
