d9f3b04d703391f3af43d7214be7ec0eb35e06ca
froth30 <theo.frohlich@gmail.com>
2017-08-13 14:49:36 -0400

    Begin CodeFights Challenge `chainReaction2`

---
+++ src/main/scala/com/codefights/ChainReaction2.scala
@@ -0,0 +1,15 @@
+package com.codefights
+
+/**
+  * CodeFights Challenge `chainReaction2`
+  *
+  * @since Aug 13, 2017
+  * @author froth30
+  */
+object ChainReaction2 {
+    
+    def chainReaction2(n: Int, m: Int, moves: Array[Array[Int]]): String = {
+        ???
+    }
+    
+}
