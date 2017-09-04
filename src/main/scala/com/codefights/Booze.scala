b31605f7f85686ff53184f323c8cdf8d1a8b6268
froth30 <theo.frohlich@gmail.com>
2017-09-03 20:01:07 -0400

    Add documentation

--- src/main/scala/com/codefights/Booze.scala
+++ src/main/scala/com/codefights/Booze.scala
@@ -2,6 +2,14 @@ package com.codefights
 
 object Booze {
     
+    /**
+      * Determines the most a person can drink and still be ok to drive to work.
+      *
+      * @param hours the number of hours until the person has to leave for work
+      * @param bacDecreasePerHour the amount by which the person's BAC decreases per hour
+      * @param bacIncreasePerDrink the amount by which the person's BAC increases per serving of their favorite drink
+      * @return how much the person can drink, such that their BAC will be at most 0.08 when they have to leave for work
+      */
     def booze(hours: Int, bacDecreasePerHour: Double, bacIncreasePerDrink: Double): Int = {
         ???
     }
