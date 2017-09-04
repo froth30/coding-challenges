742ad1878ef516e3a4dc6f918dcbbb9c8d3b8548
froth30 <theo.frohlich@gmail.com>
2017-09-03 20:01:40 -0400

    Abbreviate method arguments

--- src/main/scala/com/codefights/Booze.scala
+++ src/main/scala/com/codefights/Booze.scala
@@ -5,12 +5,12 @@ object Booze {
     /**
       * Determines the most a person can drink and still be ok to drive to work.
       *
-      * @param hours the number of hours until the person has to leave for work
-      * @param bacDecreasePerHour the amount by which the person's BAC decreases per hour
-      * @param bacIncreasePerDrink the amount by which the person's BAC increases per serving of their favorite drink
+      * @param h the number of hours until the person has to leave for work
+      * @param d the amount by which the person's BAC decreases per hour
+      * @param i the amount by which the person's BAC increases per serving of their favorite drink
       * @return how much the person can drink, such that their BAC will be at most 0.08 when they have to leave for work
       */
-    def booze(hours: Int, bacDecreasePerHour: Double, bacIncreasePerDrink: Double): Int = {
+    def booze(h: Int, d: Double, i: Double): Int = {
         ???
     }
     
