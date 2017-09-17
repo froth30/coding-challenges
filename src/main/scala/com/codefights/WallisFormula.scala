ea1209433bd3b4436d36079277b2123dbb3aef55
froth30 <theo.frohlich@gmail.com>
2017-09-17 13:49:03 -0400

    Begin CodeFights Interview Practice `wallisFormula`

---
+++ src/main/scala/com/codefights/WallisFormula.scala
@@ -0,0 +1,26 @@
+package com.codefights
+
+/**
+  * CodeFights Challenge `wallisFormula`
+  *
+  * @since  Sep 16, 2017
+  * @author froth30
+  */
+object WallisFormula {
+    
+    /**
+      * Calculates the value of the integrated function `sin` or `cos` using Wallis' formula.
+      * <br><i>
+      *     It doesn't depend on whether the function is `sin` or `cos`;
+      *     it depends on the power of the function only.
+      * </i></br>
+      *
+      * @param n  The power of the `sin` or `cos` function.
+      *           <i>Guaranteed constraints: 2 < `n` < 35</i>
+      * @return   The value of the integrated function within the bounds of `π/2` to `0`.
+      */
+    def wallisFormula(n: Int): Double = {
+        ???
+    }
+    
+}
