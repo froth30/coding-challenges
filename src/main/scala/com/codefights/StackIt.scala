0c09e72d927d78a56f8dbd4ce85ab64f68edb5c3
froth30 <theo.frohlich@gmail.com>
2017-09-23 17:47:01 -0400

    Add documentation; abbreviate parameter

--- src/main/scala/com/codefights/StackIt.scala
+++ src/main/scala/com/codefights/StackIt.scala
@@ -8,7 +8,15 @@ package com.codefights
   */
 object StackIt {
     
-    def stackIt(packs: Long): Boolean = {
+    /**
+      * Determines whether it is possible to stack a given number of packs of XYZ soda in a perfect
+      * triangular structure.
+      *
+      * @param p  The number of XYZ soda packs in stock
+      * @return   `true` if it is possible to stack `p` packs in a perfect triangular structure;
+      *           `false` if it is not possible.
+      */
+    def stackIt(p: Long): Boolean = {
         ???
     }
     
