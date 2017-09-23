a93da52d403e2c5c1c178c6da92ea57f1b20d22e
froth30 <theo.frohlich@gmail.com>
2017-09-23 16:59:10 -0400

    Begin CodeFights Challenge `stackIt`

---
+++ src/main/scala/com/codefights/StackIt.scala
@@ -0,0 +1,15 @@
+package com.codefights
+
+/**
+  * CodeFights Challenge `stackIt`
+  *
+  * @since  Sep 23, 2017
+  * @author froth30
+  */
+object StackIt {
+    
+    def stackIt(packs: Long): Boolean = {
+        ???
+    }
+    
+}
