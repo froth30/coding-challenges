2f9d5a4e331285648660c1f7f4f983ad8a9f8c7b
froth30 <theo.frohlich@gmail.com>
2017-09-23 21:36:34 -0400

    Solve CodeFights Challenge `stackIt`

--- src/main/scala/com/codefights/StackIt.scala
+++ src/main/scala/com/codefights/StackIt.scala
@@ -13,11 +13,14 @@ object StackIt {
       * triangular structure.
       *
       * @param p  The number of XYZ soda packs in stock
+      *           <br><i>Guaranteed constraints:</i>
+      *           <br>`0 ≤ packs < 10^12`
+      * @param n  The current level of XYZ stock (from the top of the structure) to verify exist
       * @return   `true` if it is possible to stack `p` packs in a perfect triangular structure;
       *           `false` if it is not possible.
       */
-    def stackIt(p: Long): Boolean = {
-        ???
-    }
+    def stackIt(p: Long, n: Int = 1): Any =
+        if (p < 1)  p == 0
+        else        stackIt(p-n, n+1)
     
 }
