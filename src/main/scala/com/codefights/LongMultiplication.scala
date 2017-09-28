745179d275985ba93d463d4ed5f9a04be958f58c
froth30 <theo.frohlich@gmail.com>
2017-09-27 22:32:15 -0400

    Improve documentation

--- src/main/scala/com/codefights/LongMultiplication.scala
+++ src/main/scala/com/codefights/LongMultiplication.scala
@@ -13,10 +13,8 @@ object LongMultiplication {
       * <i>However</i>, instead of returning the product, this method returns the number of
       * <b>carry operations</b>.
       *
-      * @param a  The first number.
-      *           <br><i>Guaranteed constraints:</i> `1 ≤ a.length < 850`.
-      * @param b  The second number.
-      *           <br><i>Guaranteed constraints:</i> `1 ≤ b.length < 850`.
+      * @param a  The first number.   <ul><i>Guaranteed constraints:</i> `1 ≤ a.length < 850`
+      * @param b  The second number.  <ul><i>Guaranteed constraints:</i> `1 ≤ b.length < 850`
       * @return   The number of carry operations performed while multiplying `a` by `b`.
       */
     def longMultiplication(a: String, b: String): Int = {
