1ec871a30ece4cf712043444b9e0be07473ea7c9
froth30 <theo.frohlich@gmail.com>
2017-09-27 20:26:29 -0400

    Begin CodeFights Challenge `longMultiplication`

---
+++ src/main/scala/com/codefights/LongMultiplication.scala
@@ -0,0 +1,26 @@
+package com.codefights
+
+/**
+  * CodeFights Challenge `longMultiplication`
+  *
+  * @since  Sep 27, 2017
+  * @author froth30
+  */
+object LongMultiplication {
+    
+    /**
+      * Multiplies two integers the good old-fashioned way.<br>
+      * <i>However</i>, instead of returning the product, this method returns the number of
+      * <b>carry operations</b>.
+      *
+      * @param a  The first number.
+      *           <br><i>Guaranteed constraints:</i> `1 ≤ a.length < 850`.
+      * @param b  The second number.
+      *           <br><i>Guaranteed constraints:</i> `1 ≤ b.length < 850`.
+      * @return   The number of carry operations performed while multiplying `a` by `b`.
+      */
+    def longMultiplication(a: String, b: String): Int = {
+        ???
+    }
+    
+}
