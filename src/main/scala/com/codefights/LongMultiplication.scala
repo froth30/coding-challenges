acc80385fefa8a7e844750b002ad02a316601cd1
froth30 <theo.frohlich@gmail.com>
2017-09-29 18:59:39 -0400

    Defer completion

--- src/main/scala/com/codefights/LongMultiplication.scala
+++ src/main/scala/com/codefights/LongMultiplication.scala
@@ -8,6 +8,9 @@ package com.codefights
   */
 object LongMultiplication {
     
+    type S = String
+    type I = Int
+    
     /**
       * Multiplies two integers the good old-fashioned way.<br>
       * <i>However</i>, instead of returning the product, this method returns the number of
@@ -17,7 +20,14 @@ object LongMultiplication {
       * @param b  The second number.  <ul><i>Guaranteed constraints:</i> `1 ≤ b.length < 850`
       * @return   The number of carry operations performed while multiplying `a` by `b`.
       */
-    def longMultiplication(a: String, b: String): Int = {
+    def longMultiplication(a: S, b: S): I = {
+        m(a toInt, b toInt, 0)
+    }
+    
+    def m(a: I, b: I, c: I): I = {
+        val u = a * b % 10
+        
+        
         ???
     }
     
