e2b24ca91fae2a4c5b49767e279bc659def4f306
froth30 <theo.frohlich@gmail.com>
2017-09-29 21:10:50 -0400

    Begin CodeFights Challenge `fibFib`

---
+++ src/main/scala/com/codefights/FibFib.scala
@@ -0,0 +1,22 @@
+package com.codefights
+
+/**
+  * CodeFights Challenge `fibFib`, a __reverse challenge__!
+  *
+  * @since  Sep 29, 2017
+  * @author froth30
+  */
+object FibFib {
+    
+    //TODO: See below...
+    /**
+      * TODO: ____
+      *
+      * @param ff  TODO: ____  <ul><i>Guaranteed constraints:</i> &#9 `0 ≤ ff ≤ 10`^`8`^
+      * @return
+      */
+    def fibFib(ff: Int): Boolean = {
+        ???
+    }
+    
+}
