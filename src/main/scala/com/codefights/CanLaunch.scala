475fdf117c2ca34bd572cdba6befd1da01a59597
froth30 <theo.frohlich@gmail.com>
2017-09-30 17:14:07 -0400

    Begin CodeFights Challenge `canLaunch`

---
+++ src/main/scala/com/codefights/CanLaunch.scala
@@ -0,0 +1,22 @@
+package com.codefights
+
+/**
+  * CodeFights Challenge `canLaunch`
+  *
+  * @since  Sep 30, 2017
+  * @author froth30
+  */
+object CanLaunch {
+    
+    /**
+      * Determines whether it is possible for your friend, Kal, to launch missiles at the squadron
+      * of alien ships, approaching in their favorite attack formation, <b><i>i-cragntbany</i></b>.
+      *
+      * @param numberOfShips  The number of alien ships in the incoming attack squadron.
+      * @return  `true` if it is possible for Kal to launch missiles at aliens; `false` otherwise.
+      */
+    def canLaunch(numberOfShips: Int): Boolean = {
+        ???
+    }
+    
+}
