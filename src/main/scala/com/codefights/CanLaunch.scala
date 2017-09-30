257b787a73335cafd901ccc7ac2be483b29ff630
froth30 <theo.frohlich@gmail.com>
2017-09-30 19:49:07 -0400

    Add second, default parameter

--- src/main/scala/com/codefights/CanLaunch.scala
+++ src/main/scala/com/codefights/CanLaunch.scala
@@ -12,12 +12,14 @@ object CanLaunch {
       * Determines whether it is possible for your friend, Kal, to launch missiles at the squadron
       * of alien ships, approaching in their favorite attack formation, <b><i>i-cragntbany</i></b>.
       *
-      * @param n  The number of alien ships in the incoming attack squadron
+      * @param p  The number of alien ships in the incoming attack squadron
       *           <ul><i>Guaranteed constraints:</i> &#9 `1 ≤ n ≤ 10`^`9`^
-      * @return   `true` if it is possible for Kal to launch missiles at the aliens;
+      * @param n  The theoretical number of alien ships along one edge of the incoming attack
+      *           squadron, assuming they are in full <i>i-cragntbany</i> formation
+      * @return   `true` if the number of incoming alien ships is a <i>pentagonal number;</i>
       *           `false` otherwise.
       */
-    def canLaunch(n: Int): Any = {
+    def canLaunch(p: Int, n: Int = 1): Nothing = {
         ???
     }
     
