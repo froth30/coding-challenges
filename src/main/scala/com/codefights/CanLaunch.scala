edeb3f3cc664c4d278939d85d09b6d58c5f3c286
froth30 <theo.frohlich@gmail.com>
2017-09-30 17:21:45 -0400

    Abbreviate parameter; complete documentation

--- src/main/scala/com/codefights/CanLaunch.scala
+++ src/main/scala/com/codefights/CanLaunch.scala
@@ -12,10 +12,12 @@ object CanLaunch {
       * Determines whether it is possible for your friend, Kal, to launch missiles at the squadron
       * of alien ships, approaching in their favorite attack formation, <b><i>i-cragntbany</i></b>.
       *
-      * @param numberOfShips  The number of alien ships in the incoming attack squadron.
-      * @return  `true` if it is possible for Kal to launch missiles at aliens; `false` otherwise.
+      * @param n  The number of alien ships in the incoming attack squadron
+      *           <ul><i>Guaranteed constraints:</i> &#9 `1 ≤ n ≤ 10`^`9`^
+      * @return   `true` if it is possible for Kal to launch missiles at the aliens;
+      *           `false` otherwise.
       */
-    def canLaunch(numberOfShips: Int): Boolean = {
+    def canLaunch(n: Int): Any = {
         ???
     }
     
