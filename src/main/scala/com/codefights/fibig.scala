00d1e2d3c60b7822277c90fd4617755b7c0d86ca
froth30 <theo.frohlich@gmail.com>
2017-10-05 23:33:04 -0400

    Begin CodeFights Challenge `fibig`

---
+++ src/main/scala/com/codefights/fibig.scala
@@ -0,0 +1,19 @@
+package com.codefights
+
+/** CodeFights Challenge `fibig`, meant to be a __reverse challenge__...
+  * 
+  * @author froth30
+  * @since  Oct 05, 2017
+  */
+object fibig {
+    
+    /** Description
+      *
+      * @param n  Definition
+      * @return   Something
+      */
+    def fibig(n: String): Int = {
+        ???
+    }
+    
+}
