c13c419978fe35790d336c9e56030b50ce970d3f
froth30 <theo.frohlich@gmail.com>
2017-10-08 15:15:13 -0400

    Begin CodeFights Challenge `concatenationProcess`

---
+++ src/main/scala/com/codefights/concatenationProcess.scala
@@ -0,0 +1,19 @@
+package com.codefights
+
+/** CodeFights Challenge `concatenationProcess`
+  * 
+  * @author froth30
+  * @since  Oct 08, 2017
+  */
+object concatenationProcess {
+    
+    /** Description
+      *
+      * @param i  Definition
+      * @return   Something
+      */
+    def concatenationProcess(i: Array[String]): String = {
+        ???
+    }
+    
+}
