757478491f76712d2f14d5a9b5d20bab97aab311
froth30 <theo.frohlich@gmail.com>
2017-10-09 04:25:41 -0400

    Begin CodeFights Challenge `truncateString`

---
+++ src/main/scala/com/codefights/truncateString.scala
@@ -0,0 +1,26 @@
+package com.codefights
+
+/** CodeFights Challenge `truncateString`
+  * 
+  * @author froth30
+  * @since  Oct 09, 2017
+  */
+object truncateString {
+    
+    /** For a given string, finds the result of applying the following sequential operation to it:
+      * <ul>
+      *     <li>if the leftmost digit of the string is divisible by `3`, remove it from the string;
+      *     <li>otherwise, if the rightmost digit of the string is divisible by `3`, remove it from
+      *         the string;
+      *     <li>otherwise, if the sum of two digits on the sides of the string is divisible by `3`,
+      *         remove <i>both digits</i> from the string;
+      * </ul>
+      *
+      * @param s  A string &#9 &#9 <i>Guaranteed constraints:</i> &#9 `1 ≤ s.length ≤ 15`
+      * @return   The result of applying the given algorithm to `s`.
+      */
+    def truncateString(s: String): String = {
+        ???
+    }
+    
+}
