895ab5558260bcec3005363a115a7320048d5124
froth30 <theo.frohlich@gmail.com>
2017-10-09 04:31:38 -0400

    Amend documentation

--- src/main/scala/com/codefights/truncateString.scala
+++ src/main/scala/com/codefights/truncateString.scala
@@ -7,7 +7,7 @@ package com.codefights
   */
 object truncateString {
     
-    /** For a given string, finds the result of applying the following sequential operation to it:
+    /** For a given string, finds the result of applying the following operation to it:
       * <ul>
       *     <li>if the leftmost digit of the string is divisible by `3`, remove it from the string;
       *     <li>otherwise, if the rightmost digit of the string is divisible by `3`, remove it from
@@ -17,7 +17,9 @@ object truncateString {
       * </ul>
       *
       * @param s  A string &#9 &#9 <i>Guaranteed constraints:</i> &#9 `1 ≤ s.length ≤ 15`
-      * @return   The result of applying the given algorithm to `s`.
+      * @return   The result of applying the algorithm to `s`, whereby the operation defined above
+      *           is applied sequentially until `s` is either (1) empty or (2) none of the three
+      *           given conditions are met.
       */
     def truncateString(s: String): String = {
         ???
