38ceb2cfb3099fc39f3b4786a3110660a7570337
froth30 <theo.frohlich@gmail.com>
2017-10-15 17:02:49 -0400

    Begin CodeFights Challenge `magicArrows`

---
+++ src/main/scala/com/codefights/magicArrows.scala
@@ -0,0 +1,23 @@
+package com.codefights
+
+/** CodeFights Challenge `magicArrows`
+  * 
+  * @author froth30
+  * @since  Oct 15, 2017
+  */
+object magicArrows {
+    
+    /** Calculates the average number of shots required to slay a villain, based on his hitpoints
+      * as well as those of his minions.
+      *
+      * @param v  Hitpoints of the villain
+      *           <ul>''Guaranteed constraints:'' &#9 `1 ≤ villain ≤ 100`
+      * @param m  Hitpoints of the villain's minions
+      *           <ul>''Guaranteed constraints:'' &#9 `0 ≤ minions.length ≤ 50`, `1 ≤ minions[i] ≤ 100`
+      * @return   The average number of ''iArrows'' required to kill the villain.
+      */
+    def magicArrows(v: Int, m: Array[Int]): Double = {
+        ???
+    }
+    
+}
