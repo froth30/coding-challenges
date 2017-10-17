5b874b31795798d1522752a061f182d5129fc276
froth30 <theo.frohlich@gmail.com>
2017-10-16 22:42:24 -0400

    Begin CodeFights Challenge `lazyFriends`

---
+++ src/main/scala/com/codefights/lazyFriends.scala
@@ -0,0 +1,21 @@
+package com.codefights
+
+/** CodeFights Challenge `lazyFriends`
+  * 
+  * @author froth30
+  * @since  Oct 16, 2017
+  */
+object lazyFriends {
+    
+    /** Determines the number of friends each person would be willing to visit.
+      *
+      * @param h  Coordinates of points where the friends live (in meters)
+      * @param m  The maximum distance at which they are willing to travel
+      * @return   An array representing the number of friends who live within
+      *           distance `m` of each respective person in `h`.
+      */
+    def lazyFriends(h: Array[Int], m: Int): Array[Int] = {
+        ???
+    }
+    
+}
