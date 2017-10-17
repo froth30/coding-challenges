2eeb6e6b8f38fbc369d323862dcd3fec3552970c
froth30 <theo.frohlich@gmail.com>
2017-10-16 23:10:55 -0400

    List guaranteed constraints

--- src/main/scala/com/codefights/lazyFriends.scala
+++ src/main/scala/com/codefights/lazyFriends.scala
@@ -10,7 +10,12 @@ object lazyFriends {
     /** Determines the number of friends each person would be willing to visit.
       *
       * @param h  Coordinates of points where the friends live (in meters)
+      *           <ul>''Guaranteed constraints:'' &#9
+      *               `1 ≤ houses.length ≤ 10`^`4`^,
+      *               `-10`^`4`^` ≤ houses[i] ≤ 10`^`4`^
       * @param m  The maximum distance at which they are willing to travel
+      *           <ul>''Guaranteed constraints:'' &#9
+      *               `2 ≤ maxDist ≤ 10`^`5`^
       * @return   An array representing the number of friends who live within
       *           distance `m` of each respective person in `h`.
       */
