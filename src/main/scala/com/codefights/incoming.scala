c7a3627d499fba80351f4c58ee5fef540be81cd7
froth30 <theo.frohlich@gmail.com>
2017-10-21 16:05:32 -0400

    Begin CodeFights Challenge `incoming`

---
+++ src/main/scala/com/codefights/incoming.scala
@@ -0,0 +1,14 @@
+package com.codefights
+
+/** CodeFights Challenge `incoming`
+  * 
+  * @author froth30
+  * @since  Oct 21, 2017
+  */
+object incoming {
+    
+    def incoming(terrain: Array[Array[Double]], vX: Double, vY: Double, h: Double): Array[Double] = {
+        ???
+    }
+    
+}
