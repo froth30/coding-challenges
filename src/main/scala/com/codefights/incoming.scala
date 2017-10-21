450ce2318a3ce6bdbb97f2d9a0859bd3ebd5e400
froth30 <theo.frohlich@gmail.com>
2017-10-21 18:01:38 -0400

    Add documentation

--- src/main/scala/com/codefights/incoming.scala
+++ src/main/scala/com/codefights/incoming.scala
@@ -10,6 +10,15 @@ object incoming {
     type A[T] = Array[T]
     type D    = Double
     
+    /** Calculates the impact location of an incoming ballistic projectile,
+      * launched over a certain terrain.
+      *
+      * @param t  The terrain, expressed as a matrix of xy-coordinate pairs
+      * @param x  The initial velocity along the horizontal axis
+      * @param y  The initial velocity along the vertical axis
+      * @param h  The initial release height
+      * @return   The xy-coordinate of impact with the terrain.
+      */
     def incoming(t: A[A[D]], x: D, y: D, h: D): A[D] = {
         ???
     }
