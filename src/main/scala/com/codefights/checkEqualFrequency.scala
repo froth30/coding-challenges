3d353d1cf6413a9a4c2da2bbe8dab59652f2829a
froth30 <theo.frohlich@gmail.com>
2017-10-22 16:35:13 -0400

    Add documentation

--- src/main/scala/com/codefights/checkEqualFrequency.scala
+++ src/main/scala/com/codefights/checkEqualFrequency.scala
@@ -7,6 +7,13 @@ package com.codefights
   */
 object checkEqualFrequency {
     
+    /** Checks whether each integer in the given array is contained the same
+      * number of times as every other integer.
+      *
+      * @param a  An array of integers
+      * @return   `true` if each unique integer is contained the same number
+      *           of times; `false` otherwise.
+      */
     def checkEqualFrequency(a: Array[Int]): Boolean = {
         ???
     }
