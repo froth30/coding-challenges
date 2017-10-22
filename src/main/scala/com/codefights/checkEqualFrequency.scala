f30441b3f56c88b998eb8cf178af96d44bc37f41
froth30 <theo.frohlich@gmail.com>
2017-10-22 16:31:36 -0400

    Begin CodeFights Challenge `checkEqualFrequency`

---
+++ src/main/scala/com/codefights/checkEqualFrequency.scala
@@ -0,0 +1,14 @@
+package com.codefights
+
+/** CodeFights Challenge `checkEqualFrequency`
+  * 
+  * @author froth30
+  * @since  Oct 22, 2017
+  */
+object checkEqualFrequency {
+    
+    def checkEqualFrequency(a: Array[Int]): Boolean = {
+        ???
+    }
+    
+}
