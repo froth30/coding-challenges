e4a38a0e10fc204461f240a940c1573d496be727
froth30 <theo.frohlich@gmail.com>
2017-11-04 01:20:20 -0400

    Begin CodeFights Challenge `addDigits`

---
+++ src/main/scala/com/codefights/addDigits.scala
@@ -0,0 +1,14 @@
+package com.codefights
+
+/** CodeFights Challenge `addDigits`
+  * 
+  * @author froth30
+  * @since  Nov 03, 2017
+  */
+object addDigits {
+    
+    def addDigits(a: Int, b: Int, n: Int): String = {
+        ???
+    }
+    
+}
