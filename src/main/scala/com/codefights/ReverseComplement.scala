f547e6e90b40c0e9c47701dc1a4eb91745bb4389
froth30 <theo.frohlich@gmail.com>
2017-11-10 00:21:34 -0500

    Add documentation

--- src/main/scala/com/codefights/ReverseComplement.scala
+++ src/main/scala/com/codefights/ReverseComplement.scala
@@ -7,6 +7,11 @@ package com.codefights
   */
 object ReverseComplement {
     
+    /** Finds the complement of a given strand of DNA.
+      *
+      * @param s  A strand of DNA
+      * @return   The complement of strand `s`, in reverse order.
+      */
     def ReverseComplement(s: String) = s.reverseMap {
         case  65 |  97  => "T"  // A
         case  67 |  99  => "G"  // C
