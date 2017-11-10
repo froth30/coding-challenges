9e5ba73c480f44f72f1a0557c9826b5b1099fd11
froth30 <theo.frohlich@gmail.com>
2017-11-09 23:56:24 -0500

    Begin CodeFights Challenge `ReverseComplement`

---
+++ src/main/scala/com/codefights/ReverseComplement.scala
@@ -0,0 +1,14 @@
+package com.codefights
+
+/** CodeFights Challenge `ReverseComplement`
+  * 
+  * @author froth30
+  * @since  Nov 09, 2017
+  */
+object ReverseComplement {
+    
+    def ReverseComplement(s: String): String = {
+        ???
+    }
+    
+}
