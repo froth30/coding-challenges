13f9a3d3a5570a81e05327ccc2c40229bbee85af
froth30 <theo.frohlich@gmail.com>
2017-11-10 00:21:22 -0500

    Achieve solution

--- src/main/scala/com/codefights/ReverseComplement.scala
+++ src/main/scala/com/codefights/ReverseComplement.scala
@@ -7,8 +7,12 @@ package com.codefights
   */
 object ReverseComplement {
     
-    def ReverseComplement(s: String): String = {
-        ???
-    }
+    def ReverseComplement(s: String) = s.reverseMap {
+        case  65 |  97  => "T"  // A
+        case  67 |  99  => "G"  // C
+        case  71 | 103  => "C"  // G
+        case  84 | 116  => "A"  // T
+        case     _      => "N"  // N
+    } mkString
     
 }
