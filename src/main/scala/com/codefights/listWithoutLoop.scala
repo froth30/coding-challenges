63eb23328cdd83d00a821d38cb37bd87526bf698
froth30 <theo.frohlich@gmail.com>
2017-11-11 13:25:26 -0500

    Begin CodeFights Challenge `listWithoutLoop`

---
+++ src/main/scala/com/codefights/listWithoutLoop.scala
@@ -0,0 +1,14 @@
+package com.codefights
+
+/** CodeFights Challenge `listWithoutLoop`
+  * 
+  * @author froth30
+  * @since  Nov 11, 2017
+  */
+object listWithoutLoop {
+    
+    def listWithoutLoop(n: Int): Array[Int] = {
+        ???
+    }
+    
+}
