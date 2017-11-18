1e23944956dfebbc296112f2cd2d085a01546631
froth30 <theo.frohlich@gmail.com>
2017-11-18 16:15:00 -0500

    Begin CodeFights Challenge `factorialsProductTrailingZeros`

---
+++ src/main/scala/com/codefights/factorialsProductTrailingZeros.scala
@@ -0,0 +1,14 @@
+package com.codefights
+
+/** CodeFights Challenge `factorialsProductTrailingZeros`
+  * 
+  * @author froth30
+  * @since  Nov 18, 2017
+  */
+object factorialsProductTrailingZeros {
+    
+    def factorialsProductTrailingZeros(l: Int, r: Int): Int = {
+        ???
+    }
+    
+}
