35bd98b1fe136975fa0c337d984b99716a879931
froth30 <theo.frohlich@gmail.com>
2017-11-19 22:28:31 -0500

    Adopt solution

--- src/main/scala/com/codefights/factorialsProductTrailingZeros.scala
+++ src/main/scala/com/codefights/factorialsProductTrailingZeros.scala
@@ -7,6 +7,17 @@ package com.codefights
   */
 object factorialsProductTrailingZeros {
     
+    /** Finds the number of trailing zeros in the decimal representation of
+      * <p>
+      *     `l! * (l + 1)! * ... * r!`
+      * </p>
+      *
+      * @param l  The left (lower) bound of the range
+      *           <ul>''Guaranteed constraints: '' &#9 `4 ≤ l ≤ r`
+      * @param r  The right (upper) bound of the range
+      *           <ul>''Guaranteed constraints: '' &#9 `l ≤ r ≤ 35`
+      * @return   The number of trailing zeros in the result.
+      */
     def factorialsProductTrailingZeros(l: Int, r: Int) =
         l to r map (x => x/5 + x/25) sum
     
