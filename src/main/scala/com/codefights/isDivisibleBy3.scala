290d6a0223718c85fd3d1c3a253cc21a8d2a868b
froth30 <theo.frohlich@gmail.com>
2017-11-21 23:24:35 -0500

    Begin CodeFights Challenge `isDivisibleBy3`

---
+++ src/main/scala/com/codefights/isDivisibleBy3.scala
@@ -0,0 +1,14 @@
+package com.codefights
+
+/** CodeFights Challenge `isDivisibleBy3`
+  * 
+  * @author froth30
+  * @since  Nov 21, 2017
+  */
+object isDivisibleBy3 {
+    
+    def isDivisibleBy3(s: String): Array[String] = {
+        ???
+    }
+    
+}
