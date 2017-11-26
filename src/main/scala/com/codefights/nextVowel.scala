24e394e4708e9ec9f19ab18aca2b93064b07a614
froth30 <theo.frohlich@gmail.com>
2017-11-26 17:09:50 -0500

    Begin CodeFights Challenge `nextVowel`

---
+++ src/main/scala/com/codefights/nextVowel.scala
@@ -0,0 +1,14 @@
+package com.codefights
+
+/** CodeFights Challenge `nextVowel`
+  * 
+  * @author froth30
+  * @since  Nov 26, 2017
+  */
+object nextVowel {
+    
+    def nextVowel(v: Char): Char = {
+        ???
+    }
+    
+}
