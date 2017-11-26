02e4a3bfdec33e522586369d8abb03053c29fa88
froth30 <theo.frohlich@gmail.com>
2017-11-25 19:03:53 -0500

    Begin CodeFights Challenge `isPangram`

---
+++ src/main/scala/com/codefights/isPangram.scala
@@ -0,0 +1,14 @@
+package com.codefights
+
+/** CodeFights Challenge `isPangram`
+  * 
+  * @author froth30
+  * @since  Nov 25, 2017
+  */
+object isPangram {
+    
+    def isPangram(s: String): Boolean = {
+        ???
+    }
+    
+}
