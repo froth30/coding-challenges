4c4093eb9ea1aa2390476d379444c8fc49fd5713
froth30 <theo.frohlich@gmail.com>
2017-11-26 03:02:23 -0500

    Add documentation

--- src/main/scala/com/codefights/isPangram.scala
+++ src/main/scala/com/codefights/isPangram.scala
@@ -7,6 +7,13 @@ package com.codefights
   */
 object isPangram {
     
+    /** Determines whether the given string is a pangram or not.
+      *
+      * @param s  A string containing characters with their ASCII-codes in
+      *           the range `[32, 126]`
+      *           <ul>''Guaranteed constraints:'' &#9 `1 ≤ s.length ≤ 100`
+      * @return   `true` if `s` is a pangram; `false` otherwise.
+      */
     def isPangram(s: String) = 65 to 90 forall (s.toUpperCase contains _)
     
 }
