8ef77b99aad5dc9e1bac1881177977b1d22e5b23
froth30 <theo.frohlich@gmail.com>
2017-11-27 00:22:12 -0500

    Achieve shortest submittable solution

--- src/main/scala/com/codefights/nextVowel.scala
+++ src/main/scala/com/codefights/nextVowel.scala
@@ -13,6 +13,6 @@ object nextVowel {
       *           <ul>''Guaranteed constraints:'' &#9 `v ∈ ['A', 'E', 'I', 'O', 'U']`
       * @return   The next English vowel, following alphabetical order.
       */
-    def nextVowel(v: Int) = "E   I   O     U     A" (v - 65)
+    def nextVowel(v:Int)="                                                        E   I   O     U     A"(v-9)
     
 }
