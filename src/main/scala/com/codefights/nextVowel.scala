cfbbaee697ef3bbbd7def6e2745fdbc7965ea0b4
froth30 <theo.frohlich@gmail.com>
2017-11-27 00:11:40 -0500

    Add documentation

--- src/main/scala/com/codefights/nextVowel.scala
+++ src/main/scala/com/codefights/nextVowel.scala
@@ -7,6 +7,12 @@ package com.codefights
   */
 object nextVowel {
     
+    /** Find the next vowel, given a single character.
+      *
+      * @param v  An English vowel
+      *           <ul>''Guaranteed constraints:'' &#9 `v ∈ ['A', 'E', 'I', 'O', 'U']`
+      * @return   The next English vowel, following alphabetical order.
+      */
     def nextVowel(v: Int) = "E   I   O     U     A" (v - 65)
     
 }
