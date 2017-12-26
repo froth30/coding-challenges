e901d0f42830bb8facdf619bf0af77c2a76eed01
froth30 <theo.frohlich@gmail.com>
2017-12-26 18:38:22 -0500

    Begin CodeFights Challenge `christmasThief`

---
+++ src/main/scala/com/codefights/christmasThief.scala
@@ -0,0 +1,14 @@
+package com.codefights
+
+/** CodeFights Challenge `christmasThief`
+  *
+  * @author froth30
+  * @since  Dec 26, 2017
+  */
+object christmasThief {
+
+  def christmasThief(coworkers: Array[Array[String]]): Array[String] = {
+    ???
+  }
+
+}
