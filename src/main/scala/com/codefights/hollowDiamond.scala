2e8f4c9cf8633ecd29866e77693b617f12ad986c
froth30 <theo.frohlich@gmail.com>
2017-12-31 05:23:09 -0500

    Begin CodeFights Challenge `hollowDiamond`

---
+++ src/main/scala/com/codefights/hollowDiamond.scala
@@ -0,0 +1,34 @@
+package com.codefights
+
+/** CodeFights Challenge `hollowDiamond`
+  *
+  * @author froth30
+  * @since  Dec 31, 2017
+  */
+object hollowDiamond {
+
+  /** Given an integer `n`, constructs a ''hollow diamond'' where:
+    * <ul>
+    *   <li>The constituents of the diamond are asterisks (`*`);
+    *   <li>The diagonal of the diamond equals `2n + 1`;
+    *   <li>The largest possible square of asterisks is removed from the center of the diamond;
+    *   <li>The string representation of the diamond contains spaces in place of newlines.
+    * </ul>
+    *
+    * @example  <li>For `n = 3`, the output should be:
+    *             <pre>    `hollowDiamond(n) = "      *     * * *   *       * * *       * *   *       *     * * *       *"`.
+    *             </pre>
+    *           </li>
+    *           <li>For `n = 4`, the output should be:
+    *             <pre>    `hollowDiamond(n) = "        *       * * *     * * * * *   * *       * * * * *       * * *   * *       * *     * * * * *       * * *         *"`.
+    *             </pre>
+    *           </li>
+    * @param n  An integer representing the side length of the diamond
+    *           <ul>''Guaranteed constraints:'' &#9 `0 < n ≤ 160`
+    * @return   A string representation of the ''hollow diamond'', constructed to specification.
+    */
+  def hollowDiamond(n: Int): String = {
+    ???
+  }
+
+}
