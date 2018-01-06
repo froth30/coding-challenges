6d206efaeca786141ad824ca3eff33a46b074205
froth30 <theo.frohlich@gmail.com>
2018-01-06 15:44:02 -0500

    Begin CodeFights Challenge `woodBars`

---
+++ src/main/scala/com/codefights/woodBars.scala
@@ -0,0 +1,14 @@
+package com.codefights
+
+/** CodeFights Challenge `woodBars`
+  *
+  * @author froth30
+  * @since  Jan 06, 2018
+  */
+object woodBars {
+
+  def woodBars(b: Array[Int]): Int = {
+    ???
+  }
+
+}
