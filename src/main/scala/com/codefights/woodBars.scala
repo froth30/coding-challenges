0f1a4e108bb0e19a13e6d05f2d8ff780bbd255a1
froth30 <theo.frohlich@gmail.com>
2018-01-07 12:53:22 -0500

    Achieve near solution (11/12 tests passed)

--- src/main/scala/com/codefights/woodBars.scala
+++ src/main/scala/com/codefights/woodBars.scala
@@ -8,18 +8,10 @@ package com.codefights
 object woodBars {
 
   def woodBars(b: Seq[Int]) = {
-    var (r, p, d) = (b.sorted take 2, b, 0)
-    while (r.size > 1) {
-      p = r take 2
-      d = p(1) - p(0)
+    val p = b.sorted take 2
+    val r = p.last % p(0)
 
-      p = Seq(p(0), p(0), d)
-      if (d < 1) p = p take 1
-
-      r = (p sorted) ++ (r drop 2)
-    }
-
-    r(0)
+    if (r > 0) r else p(0)
   }
 
 }
