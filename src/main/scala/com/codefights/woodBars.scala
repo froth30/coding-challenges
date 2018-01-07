5ceea313a74e658548abc481e4431aaf93e3c441
froth30 <theo.frohlich@gmail.com>
2018-01-06 21:28:29 -0500

    Achieve faster solution

--- src/main/scala/com/codefights/woodBars.scala
+++ src/main/scala/com/codefights/woodBars.scala
@@ -8,15 +8,15 @@ package com.codefights
 object woodBars {
 
   def woodBars(b: Seq[Int]) = {
-    var (r, p, d) = (b, b, 8)
+    var (r, p, d) = (b.sorted take 2, b, 0)
     while (r.size > 1) {
-      p = r.sorted take 2
+      p = r take 2
       d = p(1) - p(0)
 
       p = Seq(p(0), p(0), d)
       if (d < 1) p = p take 1
 
-      r = (r drop 2) ++ p
+      r = (p sorted) ++ (r drop 2)
     }
 
     r(0)
