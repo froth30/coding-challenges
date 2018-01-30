dd46fd6e3cdc3b6f01bf389d8d302cf0e3dddbb1
froth30 <theo.frohlich@gmail.com>
2018-01-29 23:32:37 -0500

    Begin CodeFights Challenge `iterMask`

---
+++ src/main/scala/com/codefights/iterMask.scala
@@ -0,0 +1,14 @@
+package com.codefights
+
+/** CodeFights Challenge `iterMask`
+  *
+  * @author froth30
+  * @since  Jan 29, 2018
+  */
+object iterMask {
+
+  def iterMask(n: Int): Array[Int] = {
+    ???
+  }
+
+}
