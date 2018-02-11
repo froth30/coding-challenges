07471069ae0e736790725a24384f5ad01d0aba14
froth30 <theo.frohlich@gmail.com>
2018-02-10 21:21:09 -0500

    Begin CodeFights Challenge `mSquare`

---
+++ src/main/scala/com/codefights/mSquare.scala
@@ -0,0 +1,14 @@
+package com.codefights
+
+/** CodeFights Challenge `mSquare`
+  *
+  * @author froth30
+  * @since  Feb 10, 2018
+  */
+object mSquare {
+
+  def mSquare(n: Int): Int = {
+    ???
+  }
+
+}
