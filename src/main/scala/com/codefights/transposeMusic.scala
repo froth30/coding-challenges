7f9e8eb40f53ea841369ceddd7bfb8decb4effa8
froth30 <theo.frohlich@gmail.com>
2018-03-11 18:36:29 -0400

    Begin CodeFights Challenge `transposeMusic`

---
+++ src/main/scala/com/codefights/transposeMusic.scala
@@ -0,0 +1,14 @@
+package com.codefights
+
+/** CodeFights Challenge `transposeMusic`
+  * 
+  * @author froth30
+  * @since  Mar 11, 2018
+  */
+object transposeMusic {
+
+    def transposeMusic(n: String, i: String): String = {
+        ???
+    }
+    
+}
