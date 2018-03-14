4ce964c5d0a9a461eb7e6f85e49343a794935ce8
froth30 <theo.frohlich@gmail.com>
2018-03-14 01:09:46 -0400

    Update parameters and begin implementation

--- src/main/scala/com/codefights/transposeMusic.scala
+++ src/main/scala/com/codefights/transposeMusic.scala
@@ -9,19 +9,21 @@ object transposeMusic {
 
   /** Transposes a given note up by the given musical interval.
     *
-    * @param n  The musical note to transpose.
+    * @param o  The musical note to transpose.
     *           <ul>''Guaranteed constraints:''
     *           <ul>`note = l + a` where
     *           <br>`l ∈ {'A'...'G'}` and
     *           <br>`a ∈ {'','#','##','b','bb'}`
-    * @param i  The interval by which to transpose the given note up.
+    * @param n  The interval by which to transpose the given note up.
     *           <ul>''Guaranteed constraints:''
     *           <ul>`interval = a + n` where
     *           <br>`a ∈ {'#','b',''}` and
     *           <br>`n ∈ {"1","2",..,"15"}`
     * @return   The transposed note in the same format as the note.
     */
-  def transposeMusic(n: String, i: String): String = {
+  def transposeMusic(o: String, n: String): String = {
+    var i = n toInt
+
     ???
   }
 
