c5b61f795d60ed0f1fde564c76c1c941abac584d
froth30 <theo.frohlich@gmail.com>
2018-04-27 20:36:33 -0400

    Begin CodeFights Challenge `cheapMarquee`

---
+++ src/main/scala/com/codefights/cheapMarquee.scala
@@ -0,0 +1,14 @@
+package com.codefights
+
+/** CodeFights Challenge `cheapMarquee`
+  *
+  * @author froth30
+  * @since  Apr 27, 2018
+  */
+object cheapMarquee {
+
+  def cheapMarquee(message: String, width: Int): Int = {
+    ???
+  }
+
+}
