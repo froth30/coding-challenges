799cde56c56168da9ca88cd9b799908171d77688
froth30 <theo.frohlich@gmail.com>
2018-05-06 05:01:35 -0400

    Begin CodeFights Challenge `kStepMaximization`

---
+++ src/main/scala/com/codefights/kStepMaximization.scala
@@ -0,0 +1,14 @@
+package com.codefights
+
+/** CodeFights Challenge `kStepMaximization`
+  *
+  * @author froth30
+  * @since  May 05, 2018
+  */
+object kStepMaximization {
+
+  def kStepMaximization(n: Int, k: Int): Int = {
+    ???
+  }
+
+}
