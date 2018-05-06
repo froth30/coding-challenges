725a430b81d039ed13bd5c4efa0f028d003f498d
froth30 <theo.frohlich@gmail.com>
2018-05-06 07:39:37 -0400

    Add documentation

--- src/main/scala/com/codefights/kStepMaximization.scala
+++ src/main/scala/com/codefights/kStepMaximization.scala
@@ -7,6 +7,22 @@ package com.codefights
   */
 object kStepMaximization {
 
+  /** Given an integer, determines the maximum possible number that can be obtained by performing a
+    * series of steps. At each step, one of the following operations can be applied to the number:
+    * <ul>
+    *   <li>Increase the number by `1`.
+    *   <li>Multiply the number by `2`.
+    *   <li>Rotate the number by `180` degrees, considering the number to be represented as a
+    *   seven-segment display character string. If this rotation results in a valid number, then
+    *   that valid number is the result of this operation. If this rotation results in an invalid
+    *   number, then this operation is impossible.
+    * </ul>
+    * No more than a specified number of steps are allowed to be performed.
+    *
+    * @param n  The original integer.
+    * @param k  The number of steps allowed to be performed.
+    * @return   The maximum value that can be obtained in no more than `k` steps.
+    */
   def kStepMaximization(n: Int, k: Int) = f(Set(n), k)
 
   def f(s: Set[Int], k: Int): Int = {
