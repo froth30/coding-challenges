ff283b02c2d53ad999dc73e9fdf6b8388c566df9
froth30 <theo.frohlich@gmail.com>
2018-05-06 06:59:24 -0400

    Achieve solution.

--- src/main/scala/com/codefights/kStepMaximization.scala
+++ src/main/scala/com/codefights/kStepMaximization.scala
@@ -7,8 +7,26 @@ package com.codefights
   */
 object kStepMaximization {
 
-  def kStepMaximization(n: Int, k: Int): Int = {
-    ???
+  def kStepMaximization(n: Int, k: Int) = f(Set(n), k)
+
+  def f(s: Set[Int], k: Int): Int = {
+    var t = Set(0) - 0
+
+    s foreach (n => {
+      t += n + 1
+      t += n * 2
+
+      val r = "" + n
+      if (r forall (c => !Set(51,52,55).contains(c)))
+        t += r
+          .map(c => Map(6->9, 9->6).getOrElse(c-48, c))
+          .reverse.mkString.toInt
+    })
+
+    if (k > 1)
+      f(t, k-1)
+    else
+      t max
   }
 
 }
