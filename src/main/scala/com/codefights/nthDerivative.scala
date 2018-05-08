fee266b9128ceb33270994bb03e16c29e291d5a2
froth30 <theo.frohlich@gmail.com>
2018-05-07 23:28:11 -0400

    Achieve working solution (7/9)

--- src/main/scala/com/codefights/nthDerivative.scala
+++ src/main/scala/com/codefights/nthDerivative.scala
@@ -8,7 +8,15 @@ package com.codefights
 object nthDerivative {
 
   def nthDerivative(c: Array[Int], n: Int, x: Int): Int = {
-    ???
+    if (n < 1)
+      c.indices.map(i => c(i) * math.pow(x, i)).sum toInt
+    else {
+      val d = c.indices.map(i => c(i) * i)
+      if (d isEmpty)
+        0
+      else
+        nthDerivative(d.tail toArray, n - 1, x)
+    }
   }
 
 }
