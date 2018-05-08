9ef5c26bb6208575f01664a071dfc74bd051141f
froth30 <theo.frohlich@gmail.com>
2018-05-07 21:46:45 -0400

    Begin CodeFights Challenge `nthDerivative`

---
+++ src/main/scala/com/codefights/nthDerivative.scala
@@ -0,0 +1,14 @@
+package com.codefights
+
+/** CodeFights Challenge `nthDerivative`
+  *
+  * @author froth30
+  * @since  May 07, 2018
+  */
+object nthDerivative {
+
+  def nthDerivative(c: Array[Int], n: Int, x: Int): Int = {
+    ???
+  }
+
+}
