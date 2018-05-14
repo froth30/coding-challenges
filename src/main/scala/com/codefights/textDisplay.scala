d48642bb586097756d56c1b02603638c73a14a65
froth30 <theo.frohlich@gmail.com>
2018-05-14 02:33:55 -0400

    Achieve solution

--- src/main/scala/com/codefights/textDisplay.scala
+++ src/main/scala/com/codefights/textDisplay.scala
@@ -8,7 +8,40 @@ package com.codefights
 object textDisplay {
 
   def textDisplay(k: Array[Int]): String = {
-    ???
+    var s = ""
+    var i = 0
+
+    def m(d: Int) = i = Set(Set(i+d, 0) max, s size) min
+
+    def y = s splitAt i
+
+    def t(c: Int) = {
+      val (a,b) = y
+      s = a + Map(
+        93  -> ";",
+        94  -> ",",
+        95  -> ".",
+        111 -> "'"
+      ).getOrElse(c/2, c.asInstanceOf[Char] toLower) + b
+      m(1)
+    }
+
+    k foreach {
+      case 8 =>
+        val (a,b) = y
+        s = a.dropRight(1) + b
+        m(-1)
+      case 35 => i = s size
+      case 36 => i = 0
+      case 37 => m(-1)
+      case 39 => m( 1)
+      case 46 =>
+        val (a,b) = y
+        s = a + b.drop(1)
+      case c => t(c)
+    }
+
+    s
   }
 
 }
