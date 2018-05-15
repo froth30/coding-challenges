2cdf962b57862c7295f6c25a93a36fecf2f56f08
froth30 <theo.frohlich@gmail.com>
2018-05-14 20:28:38 -0400

    Begin CodeFights Challenge `kbonacci`

---
+++ src/main/scala/com/codefights/kbonacci.scala
@@ -0,0 +1,14 @@
+package com.codefights
+
+/** CodeFights Challenge `kbonacci`
+  *
+  * @author froth30
+  * @since  May 14, 2018
+  */
+object kbonacci {
+
+  def kbonacci(k: Int, n: Int): String = {
+    ???
+  }
+
+}
