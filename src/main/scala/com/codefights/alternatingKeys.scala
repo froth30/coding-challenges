f359d392c5b28e254af2bb88cbaf262a52681ac4
froth30 <theo.frohlich@gmail.com>
2018-06-23 23:19:20 -0400

    Begin CodeFights Challenge `alternatingKeys`

---
+++ src/main/scala/com/codefights/alternatingKeys.scala
@@ -0,0 +1,14 @@
+package com.codefights
+
+/** CodeFights Challenge `alternatingKeys`
+  *
+  * @author froth30
+  * @since  Jun 23, 2018
+  */
+object alternatingKeys {
+
+  def alternatingKeys(s: String): Int = {
+    ???
+  }
+
+}
