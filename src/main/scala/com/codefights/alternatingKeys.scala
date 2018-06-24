f990b164e3bc474524968da891acf8eeb7412d97
froth30 <theo.frohlich@gmail.com>
2018-06-24 05:20:46 -0400

    Achieve viable draft

--- src/main/scala/com/codefights/alternatingKeys.scala
+++ src/main/scala/com/codefights/alternatingKeys.scala
@@ -7,8 +7,32 @@ package com.codefights
   */
 object alternatingKeys {
 
-  def alternatingKeys(s: String): Int = {
-    ???
+  val S = "                                                                            R     L              LLLLLLLRRRRRRRRRLLLLRLLLRL"
+
+  def alternatingKeys(t: String): Int = {
+    val n = t size
+    val s = (0 + t map (S(_))) toArray
+
+    if (n < 2)
+      n * 50
+
+    else {
+      val p = s.prefixLength(_ < 33) - 1
+
+      p * 50 +
+        (p to n).map(i => {
+          (s(i-1), s(i)) match {
+            case (_, 32) =>
+              s(i) = S(s(i-1))
+              50
+            case (a, b) =>
+              if (a == b)
+                75
+              else
+                50
+          }
+        }).sum
+    }
   }
 
 }
