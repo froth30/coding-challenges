f1cc1689c8775f9f0a2c41544c90a56d0170fe35
froth30 <theo.frohlich@gmail.com>
2018-07-07 17:19:16 -0400

    Commit progress

--- src/main/scala/com/codefights/kitColors.scala
+++ src/main/scala/com/codefights/kitColors.scala
@@ -7,7 +7,15 @@ package com.codefights
   */
 object kitColors {
 
+  def p(c: String, i: Int) = Integer parseInt (c.slice(i, i+2), 16)
+
   def kitColors(a: Array[String], b: Array[String]): Array[Array[String]] = {
+    val d = a.map(a => b.map(b => {
+      def c(i: Int) = (p(a,i) - p(b,i)) abs
+
+      c(1) + c(3) + c(5)
+    }))
+
     ???
   }
 
