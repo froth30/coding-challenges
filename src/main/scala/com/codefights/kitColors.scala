670f7fc19c35a2596ecb34bd21d26107d63d9e4b
froth30 <theo.frohlich@gmail.com>
2018-07-07 16:27:52 -0400

    Begin CodeFights Challenge `kitColors`

---
+++ src/main/scala/com/codefights/kitColors.scala
@@ -0,0 +1,14 @@
+package com.codefights
+
+/** CodeFights Challenge `kitColors`
+  *
+  * @author froth30
+  * @since  Jul 07, 2018
+  */
+object kitColors {
+
+  def kitColors(a: Array[String], b: Array[String]): Array[Array[String]] = {
+    ???
+  }
+
+}
