5502b39ea97a2a7c453f9192d0abb06deaf3673f
froth30 <theo.frohlich@gmail.com>
2018-07-10 00:20:55 -0400

    Begin CodeFights Challenge `possibleChampions`

---
+++ src/main/scala/com/codefights/possibleChampions.scala
@@ -0,0 +1,14 @@
+package com.codefights
+
+/** CodeFights Challenge `possibleChampions`
+  *
+  * @author froth30
+  * @since  Jul 10, 2018
+  */
+object possibleChampions {
+
+  def possibleChampions(b: Int, c: Int, e: Int, f: Int): Array[String] = {
+    ???
+  }
+
+}
