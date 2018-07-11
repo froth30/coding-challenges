2915eb07858b04f062fba150714ed952c408514b
froth30 <theo.frohlich@gmail.com>
2018-07-10 21:48:38 -0400

    Adopt shortest solution.

--- src/main/scala/com/codefights/possibleChampions.scala
+++ src/main/scala/com/codefights/possibleChampions.scala
@@ -7,8 +7,10 @@ package com.codefights
   */
 object possibleChampions {
 
-  def possibleChampions(b: Int, c: Int, e: Int, f: Int): Array[String] = {
-    ???
-  }
+  def possibleChampions(b: Int, c: Int, e: Int, f: Int) =
+    Array("Belgium" -> (b - f > 1 & c != e),
+          "Croatia" -> (c - e > 1 & b != f),
+          "England" -> (e - c > 1 & b != f),
+          "France"  -> (f - b > 1 & c != e)) filter (_ _2) map (_ _1)
 
 }
