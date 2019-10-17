0f93989f63b8e8e5060a6cd7094d47636119efdc
froth30 <theo.frohlich@gmail.com>
2019-10-17 00:25:12 -0400

    Complete CodeSignal Challenge 'sumOfMultiples' in Scala.

---
+++ src/main/scala/com/codesignal/sumOfMultiples.scala
@@ -0,0 +1,13 @@
+package com.codesignal
+
+/** CodeSignal Challenge `sumOfMultiples`
+  *
+  * @author froth30
+  * @since  Oct 17, 2019
+  */
+object sumOfMultiples {
+
+  def sumOfMultiples(n: Int, k: Int) =
+    k to n by k sum
+
+}
