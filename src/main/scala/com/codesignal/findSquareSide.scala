4ca85bc7b78a6dba9e09ef92b721762263902415
froth30 <theo.frohlich@gmail.com>
2019-10-20 23:00:37 -0400

    Achieve shorter solution, after clarification of challenge description.

--- src/main/scala/com/codesignal/findSquareSide.scala
+++ src/main/scala/com/codesignal/findSquareSide.scala
@@ -7,11 +7,9 @@ package com.codesignal
   */
 object findSquareSide {
 
-  type V = Seq[Int]
-
   def s(n: Int) = n * n
 
-  def findSquareSide(x: V, y: V) =
-    1 to 3 map (i => s(x(i) - x(0)) + s(y(i) - y(0))) min
+  def findSquareSide(x: Seq[Int], y: Seq[Int]) =
+    s(x(2) - x(1)) + s(y(2) - y(1))
 
 }
