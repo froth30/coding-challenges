7ce3f4d328c169e39d9df4d8244b5bf193fede5c
froth30 <theo.frohlich@gmail.com>
2019-11-05 20:28:27 -0500

    Complete CodeSignal Challenge 'arrayMinimumIndex' in Scala.

---
+++ src/main/scala/com/codesignal/arrayMinimumIndex.scala
@@ -0,0 +1,13 @@
+package com.codesignal
+
+/** CodeSignal Challenge `arrayMinimumIndex`
+  *
+  * @author froth30
+  * @since  Nov 05, 2019
+  */
+object arrayMinimumIndex {
+
+  def arrayMinimumIndex(a: Seq[Int]) =
+    a indexOf a.min
+
+}
