aeb55dbff75df93e6835ef50d8befc23d0b9b200
froth30 <theo.frohlich@gmail.com>
2019-11-06 23:18:28 -0500

    Complete CodeSignal Challenge 'replaceAllDigitsRegExp' in Scala.

---
+++ src/main/scala/com/codesignal/replaceAllDigitsRegExp.scala
@@ -0,0 +1,13 @@
+package com.codesignal
+
+/** CodeSignal Challenge `replaceAllDigitsRegExp`
+  *
+  * @author froth30
+  * @since  Nov 06, 2019
+  */
+object replaceAllDigitsRegExp {
+
+  def replaceAllDigitsRegExp =
+    (_: String) replaceAll ("[0-9]", "#")
+
+}
