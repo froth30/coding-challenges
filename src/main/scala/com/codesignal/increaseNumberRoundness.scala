497d7838e60d00d8f0859b53819625c77477e8f9
froth30 <theo.frohlich@gmail.com>
2019-11-15 22:03:27 -0500

    Complete CodeSignal Challenge 'increaseNumberRoundness' in Scala.

---
+++ src/main/scala/com/codesignal/increaseNumberRoundness.scala
@@ -0,0 +1,13 @@
+package com.codesignal
+
+/** CodeSignal Challenge `increaseNumberRoundness`
+  *
+  * @author froth30
+  * @since  Nov 15, 2019
+  */
+object increaseNumberRoundness {
+
+  def increaseNumberRoundness =
+    "" + (_: Int) matches ".*0[^0].*"
+
+}
